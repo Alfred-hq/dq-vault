fd7849af3184bb27adfff39f882af2d5d04458c7
dichouhan <dichouhan@deqode.com>
2023-03-22 14:54:00 +0530

    added: testcases for path_guardians.go,

--- api/monkey_patches.go
+++ api/monkey_patches.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/ryadavDeqode/dq-vault/api/helpers"
 	"github.com/undefinedlabs/go-mpatch"
 )
 
@@ -17,7 +18,7 @@ func MPatchDecodeJSON(rval error) (*mpatch.Patch, error) {
 	patch, err = mpatch.PatchInstanceMethodByName(reflect.TypeOf(&logical.StorageEntry{}), "DecodeJSON", func(arg1 *logical.StorageEntry, arg2 interface{}) error {
 		patch.Unpatch()
 		defer patch.Patch()
-		return rval 
+		return rval
 	})
 
 	if err != nil {
@@ -37,6 +38,25 @@ func MPatchGet(rval string) (*mpatch.Patch, error) {
 		patch.Unpatch()
 		defer patch.Patch()
 		return rval
+	})
+
+	if err != nil {
+		fmt.Println("patching failed", err)
+	}
+
+	return patch, err
+}
+
+//go:noinline
+func MPatchVerifyJWTSignature(rval1 bool, rval2 string) (*mpatch.Patch, error) {
+
+	var patch *mpatch.Patch
+	var err error
+
+	patch, err = mpatch.PatchMethod(helpers.VerifyJWTSignature, func(_ string, _ map[string]string, _ string, _ string) (arg1 bool, arg2 string) {
+		patch.Unpatch()
+		defer patch.Patch()
+		return rval1, rval2
 	})
 
 	if err != nil {
--- api/path_guardians_test.go
+++ api/path_guardians_test.go
@@ -4,22 +4,84 @@ import (
 	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/ryadavDeqode/dq-vault/config"
+	"github.com/ryadavDeqode/dq-vault/test/unit_test/mocks"
 )
 
-
 func TestPathGuardians(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	b := backend{}
 
 	req := logical.Request{}
 	d := framework.FieldData{}
 
-	MPatchGet("test")
-
+	p, _ := MPatchGet("test")
 
 	pathGs, err := b.pathGuardians(context.Background(), &req, &d)
 
-	
+	p.Unpatch()
 
+	if pathGs.Data["remarks"] != "return proper quest" {
+		t.Error("not the response expected!", pathGs)
+	}
+
+	if err != nil {
+		t.Error("error wasn't expected", err)
+	}
+
+	MPatchGet("GET_GUARDIANS")
+
+	s := mocks.NewMockStorage(ctrl)
+
+	req.Storage = s
+
+	s.EXPECT().Get(context.Background(), config.StorageBasePath+"GET_GUARDIANS").Return(&logical.StorageEntry{}, nil).MinTimes(0)
+	_, err = b.pathGuardians(context.Background(), &req, &d)
+
+	if err == nil {
+		t.Error("error was expected", err)
+	}
+
+	MPatchDecodeJSON(nil)
+
+	pathGs, err = b.pathGuardians(context.Background(), &req, &d)
+
+	if err != nil {
+		t.Error("no error was expected", pathGs, err)
+	}
+	testRemark := "test_remark"
+	mJWT,_ := MPatchVerifyJWTSignature(false, testRemark)
+
+	pathGs, err = b.pathGuardians(context.Background(), &req, &d)
+
+	mJWT.Unpatch()
+
+	if err != nil {
+		t.Error("no error was expected", pathGs, err)
+	}
+	if pathGs.Data["remarks"] != testRemark {
+		t.Error("unexpected return value!", pathGs)
+	}
+
+
+	MPatchVerifyJWTSignature(true, testRemark)
+
+	pathGs, err = b.pathGuardians(context.Background(), &req, &d)
+
+	if err != nil {
+		t.Error("no error was expected", pathGs, err)
+	}
+
+	if pathGs.Data["remarks"] != "success"{
+		t.Error("unexpected remarks value, expected success, received - ", pathGs)
+	}
+
+	if pathGs.Data["status"] != true {
+		t.Error("unexpected status value, expected true, received - ", pathGs)
+	}
 }
