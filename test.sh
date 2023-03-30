43d8fab6f72dccfe780d06e5970c761af47486db
Divyanshu Chouhan <dichouhan@deqode.com>
2023-03-30 17:22:18 +0530

    fix: imports of failing test cases.

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 go test -v ./... -gcflags="-l" -cover
 
 
-go test -v -coverprofile cover.out                       
-go tool cover -html cover.out -o cover.html
+#go test -v -coverprofile cover.out                       
+#go tool cover -html cover.out -o cover.html
+# cd api && open cover.html
