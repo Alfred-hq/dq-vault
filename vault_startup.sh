e8ed191843e5714dcf5ff53e96cf6f80abc99156
Ajaiswal2610 <ajaiswal@deqode.com>
2023-07-21 22:26:44 +0530

    remove sleeps

--- vault_startup.sh
+++ vault_startup.sh
@@ -4,7 +4,6 @@ set -xe
 #set -o pipefail
 echo "STARTUP SCRIPT STARTED"
 echo ${ROOT_TOKEN_KEY}
-sleep 30
 export VAULT_ADDR="http://127.0.0.1:8080"
 export SHA256=$(shasum -a 256 "/vault/plugins/vault_plugin" | cut -d' ' -f1)
 root_token="THIS_IS_DUMMY"
@@ -21,12 +20,11 @@ then
         touch chmod 777 /vault/token.txt
         echo $root_token >> /vault/token.txt
         gcloud secrets versions add ${ROOT_TOKEN_KEY} --data-file=/vault/token.txt --project=${PROJECT_ID}
-
+        sleep 30
 fi
 root_token=$(gcloud secrets versions access latest --secret=${ROOT_TOKEN_KEY} --project=${PROJECT_ID})
 if [ $(vault status | grep 'Initialized'| awk '{print $NF}') = "true" ]
 then
-        sleep 30
         vault login $root_token
         vault secrets disable /api
         vault write sys/plugins/catalog/secrets-api sha_256=$SHA256 command="vault_plugin"
@@ -38,7 +36,6 @@ fi
 
 
 ## get root token from secret in root token
-sleep 30
 vault login $root_token
 vault plugin register -sha256=$SHA256 -command="vault_plugin" -version=1 secret secrets-api
 vault secrets tune -plugin-version=1 api
